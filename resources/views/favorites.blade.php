b5d542bb865a547bc7c26ce9691252477daed601
Ayatooo <louisreynard919@gmail.com>
2023-04-13 23:00:24 +0200

    :blue_heart: favorites merged

--- resources/views/favorites.blade.php
+++ resources/views/favorites.blade.php
@@ -2,9 +2,13 @@
     <div class="py-12">
         <h1 class="container-title">Mes Favoris 🎈</h1>
         <div class="container">
-            @for($i = 0; $i < count($favoritesArr); $i++)
-                <x-recipe-card :recipe="$favoritesArr[$i]" />
-            @endfor
+            @if (count($favoritesArr) == 0)
+                <p style="color: #fff; font-size: 1.5rem; font-weight: 700; text-align: center;">Vous n'avez pas encore de favoris !</p>
+            @else
+                @for($i = 0; $i < count($favoritesArr); $i++)
+                    <x-recipe-card :recipe="$favoritesArr[$i]" />
+                @endfor
+            @endif
         </div>
     </div>
     <style>
