76f2bf30e411984e82a3ae09590ec8c371c17218
mvincent <vincentmagnienpro@gmail.com>
2023-04-14 10:49:54 +0200

    :zap: recipe finished

--- resources/views/recipe.blade.php
+++ resources/views/recipe.blade.php
@@ -10,14 +10,14 @@
             </div>
             <div class="container-cocktail-infos">
                 <div class="container-cocktail-name">
-                    <div class="cocktail-name">{{$data->strDrink}}</div>
+                    <div class="cocktail-name">{{$data->strDrink}}  <i class="fa-regular fa-star fa-mg" style="color: #010101;"></i></div>
                 </div>
                 <div class="container-cocktail-ingredients">
                     <div class="text-ingredients">Pour préparer ce cocktail, il vous faudra par verre :</div>
                     <ul class="list-ingredients">
-                        @for ($i = 1; $i <= 8; $i++)
+                        @for ($i = 1; $i <= 15; $i++)
                         @if ($data->{'strIngredient'.$i} !== null)
-                            <li>{{ $data->{'strIngredient'.$i} }}</li>
+                            <li>{{$data->{'strMeasure'.$i} }} {{ $data->{'strIngredient'.$i} }}</li>
                         @endif
                     @endfor
                     </ul>
@@ -28,15 +28,17 @@
                             Pour préparer ce cocktail, c’est super simple ! Armez vous d’un shaker et suivez les instructions ci-dessous.
                         </div>
                         <div class="infos-recipe">
-                            <ul class="list-recipe">
-                                <li>{{$data->strInstructions}}</li>
-                            </ul>
+                            <i class="fa-solid fa-square-caret-right fa-xl" style="color: #010101;"></i>
+                            <div class="instructions">
+                                {{$data->strInstructions}}
+                            </div>
                         </div>
+                        <div class="degustez">Dégustez !</div>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="container-the-alcool"></div>
+        <img class="container-the-alcool" src="https://www.thecocktaildb.com/images/ingredients/{{$data->strIngredient1}}-Medium.png">
     </div>
     <div>
 </x-app-layout>
