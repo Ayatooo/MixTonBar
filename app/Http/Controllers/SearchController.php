ede4855418e277c59d42e6fd2b72d4b630f6b9d4
Ayatooo <louisreynard919@gmail.com>
2023-04-14 11:24:11 +0200

    :zap: select2 working

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -4,34 +4,27 @@ namespace App\Http\Controllers;
 
 use GuzzleHttp\Client;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\View;
 
 class SearchController extends Controller
 {
     public function searchByStr(Request $request)
     {
+        $term = $request->input('term')['term'];
         $client = new Client();
         $response = $client->request('GET', 'https://www.thecocktaildb.com/api/json/v1/1/search.php', [
             'query' => [
-                's' => $request->input('search'),
+                's' => $term,
             ],
         ]);
         $body = $response->getBody();
         $data = (array) json_decode($body->getContents());
-        $toReturn = '';
-        if (count($data) != 0)
-        {
-            foreach($data as $key => $value)
-            {
-                if ($key == 5) {
-                    break;
-                }
-                $toReturn .= View::make(
-                    'components.recipe-card',
-                    [
-                        'recipe' => (array) $value[0],
-                    ]
-                )->render();
+        $toReturn = [];
+        if (isset($data['drinks'])) {
+            foreach ($data['drinks'] as $drink) {
+                $toReturn[] = [
+                    'id' => $drink->idDrink,
+                    'name' => $drink->strDrink,
+                ];
             }
         }
         return $toReturn;
