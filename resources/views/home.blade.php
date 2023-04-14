d404947ffd13f466a2c27d5e232cc8af90abebe3
Ayatooo <louisreynard919@gmail.com>
2023-04-14 17:37:05 +0200

    :bug: fix responsive

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,25 +2,25 @@
     <div class="py-12">
         <h1 class="container-title">Cocktails Aléatoires</h1>
         <div class="container">
-            @foreach($random as $recipe)
+            @foreach ($random as $recipe)
                 <x-recipe-card :recipe="$recipe" />
             @endforeach
         </div>
-        @if(request()->routeIs('home'))
-        <h1 class="container-title">Les Cocktails Avec Alcool</h1>
-        <div class="container">
-            @for($i = 0; $i < 10; $i++)
-                <x-recipe-card :recipe="$alcoholArr[$i]" />
-            @endfor
-        </div>
+        @if (request()->routeIs('home'))
+            <h1 class="container-title">Les Cocktails Avec Alcool</h1>
+            <div class="container">
+                @for ($i = 0; $i < 10; $i++)
+                    <x-recipe-card :recipe="$alcoholArr[$i]" />
+                @endfor
+            </div>
         @endif
-        @if(request()->routeIs('noalcohol'))
-        <h1 class="container-title">Les Cocktails Sans Alcool</h1>
-        <div class="container">
-            @for($i = 0; $i < 10; $i++)
-                <x-recipe-card :recipe="$nonAlcoholArr[$i]" />
-            @endfor
-        </div>
+        @if (request()->routeIs('noalcohol'))
+            <h1 class="container-title">Les Cocktails Sans Alcool</h1>
+            <div class="container">
+                @for ($i = 0; $i < 10; $i++)
+                    <x-recipe-card :recipe="$nonAlcoholArr[$i]" />
+                @endfor
+            </div>
         @endif
     </div>
     <style>
@@ -41,7 +41,6 @@
             display: flex;
             flex-wrap: wrap;
             justify-content: space-between;
-            margin: 0 5%;
         }
-        </style>
+    </style>
 </x-app-layout>
